bf33b68ced7059466b3758c004ff100207bafec2
AP <amalpn007@gmail.com>
2020-08-05 18:21:37 +0530

    project basics

---
+++ src/main/java/com/ap/sample/springsecurity/SpringSecuritySample/SpringSecuritySampleApplication.java
@@ -0,0 +1,13 @@
+package com.ap.sample.springsecurity.SpringSecuritySample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySampleApplication.class, args);
+	}
+
+}
