2a2835fa8b91b6452832bfe3fd2355c698952073
AP <amalpn007@gmail.com>
2020-08-25 23:22:29 +0530

    JDBC authentication and authorization implemented using in memory db (H2) with simple JDBC.

--- src/main/java/com/ap/sample/springsecurity/SpringSecuritySample/main/web/config/security/authentication/AuthenticationConfiguration.java
+++ src/main/java/com/ap/sample/springsecurity/SpringSecuritySample/main/web/config/security/authentication/AuthenticationConfiguration.java
@@ -1,23 +1,40 @@
 package com.ap.sample.springsecurity.SpringSecuritySample.main.web.config.security.authentication;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @EnableWebSecurity
 public class AuthenticationConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	DataSource dataSource;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		auth.inMemoryAuthentication()
-				.withUser("user")
-				.password("user")
-				.roles("USER");
+		auth.jdbcAuthentication()
+			.dataSource(dataSource);
+			
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/admin").hasRole("ADMIN")
+			.antMatchers("/user").hasAnyRole("ADMIN", "USER")
+			.antMatchers("/").permitAll()
+			.and().formLogin();
+			
 	}
 	
 	@Bean
--- src/main/java/com/ap/sample/springsecurity/SpringSecuritySample/main/web/controller/MainWebController.java
+++ src/main/java/com/ap/sample/springsecurity/SpringSecuritySample/main/web/controller/MainWebController.java
@@ -10,4 +10,14 @@ public class MainWebController {
 		return "Spring boot app is working";
 	}
 
+	@GetMapping("/user") 
+	public String user() {
+		return "User has logged in";
+	}
+	
+	@GetMapping("/admin")
+	public String admin() {
+		return "Admin has logged in";
+	}
+	
 }
